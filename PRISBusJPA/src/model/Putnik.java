a56506ba02c74326d8a36fa3c13b026306787a12
nixos89 <nixos89@gmail.com>
2017-04-17 11:33:30 +0200

    @TK-756: Izmenjeni Bean-ovi za Many-To-Many vezu

--- PRISBusJPA/src/model/Putnik.java
+++ PRISBusJPA/src/model/Putnik.java
@@ -2,6 +2,8 @@ package model;
 
 import java.io.Serializable;
 import javax.persistence.*;
+
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -41,6 +43,7 @@ public class Putnik implements Serializable {
 	private List<Polazak> polazaks;
 
 	public Putnik() {
+		polazaks = new ArrayList<Polazak>();
 	}
 
 	public int getIdputnik() {
