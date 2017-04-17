e18ec9c29c4754d65e930208863a9fb4139f200a
nixos89 <nixos89@gmail.com>
2017-04-17 11:55:55 +0200

    @master: izmenjeni Java Bean-ovi zbog Many-To-Many veze

--- PRISBusJPA/src/model/Putnik.java
+++ PRISBusJPA/src/model/Putnik.java
@@ -1,8 +1,18 @@
 package model;
 
 import java.io.Serializable;
-import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
 
 
 /**
@@ -41,6 +51,7 @@ public class Putnik implements Serializable {
 	private List<Polazak> polazaks;
 
 	public Putnik() {
+		polazaks = new ArrayList<Polazak>();
 	}
 
 	public int getIdputnik() {
