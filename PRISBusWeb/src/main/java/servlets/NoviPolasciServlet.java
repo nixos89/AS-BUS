e247e93c29d643e41cfb3695590d5903e47fb02c
nixos89 <nixos89@gmail.com>
2017-04-19 16:00:50 +0200

    @TK-765: Dodata opcija za selekciju vrsta polazaka

---
+++ PRISBusWeb/src/main/java/servlets/NoviPolasciServlet.java
@@ -0,0 +1,32 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class NoviPolasciServlet
+ */
+public class NoviPolasciServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public NoviPolasciServlet() {
+        super();
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
