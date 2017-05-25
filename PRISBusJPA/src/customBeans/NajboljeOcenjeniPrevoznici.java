3698ea5148279bf3bec19a38236396dd63400cf4
nixos89 <nixos89@gmail.com>
2017-05-25 21:32:44 +0200

    @master: merge-ovan sa TK-1115

--- PRISBusJPA/src/customBeans/NajboljeOcenjeniPrevoznici.java
+++ PRISBusJPA/src/customBeans/NajboljeOcenjeniPrevoznici.java
@@ -11,7 +11,7 @@ public class NajboljeOcenjeniPrevoznici {
 
 	public NajboljeOcenjeniPrevoznici() {		
 		najboljeOcenjeniPrevoznici = PrevoznikManager.najboljeOcenjeniPrevoznici();
-	}
+	} 
 
 	public Map<Prevoznik, Double> getNajboljeOcenjeniPrevoznici() {
 		return najboljeOcenjeniPrevoznici;
